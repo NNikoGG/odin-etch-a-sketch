0f8edc409860a3713bb4340c75b2e539c1944caf
NNikoGG <geekgaming222@gmail.com>
2024-01-03 15:17:22 +0530

    Added two buttons to select color and a clear button to clear the grid

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,24 @@
 
 const squareDivsContainer = document.querySelector('div.container');
 const sizeButton = document.querySelector('div.size-button button');
+const blackButton = document.getElementById('blackButton');
+const rainbowButton = document.getElementById('rainbowButton');
+const clearButton = document.getElementById('clearButton');
+
+// setting a default color mode
+let colorMode = 'black';
+// setting a default grid dimension
+let size = 16;
+
+blackButton.addEventListener('click', () => colorMode = 'black');
+rainbowButton.addEventListener('click', () => colorMode = 'rainbow');
 sizeButton.addEventListener('click', sizeButtonClicked);
+clearButton.addEventListener('click', () => createGrid(size));
 
 function sizeButtonClicked(){
-    let size = '';
     size = prompt("Enter the size of the square grid (For eg: 16, 32 etc");
     size = parseInt(size);
+
     if (isNaN(size)){
         console.log("Error! Please enter a valid numerical size");
         sizeButtonClicked();
@@ -23,20 +35,28 @@ function sizeButtonClicked(){
 
 function createGrid(size) {
     squareDivsContainer.innerHTML = '';
+    // Getting the size of each individual squares in the grid
     const squareSize = squareDivsContainer.clientWidth / size;
 
     for (let i = 0; i < size * size; i++) {
         const squareDiv = document.createElement('div');
         squareDiv.className = "square-div";
         squareDiv.style.flexBasis = `${squareSize}px`;
-        squareDiv.style.height = `${squareSize}px`; // Set height to maintain aspect ratio
-
-        squareDiv.onmouseover = function() {
-            squareDiv.style.backgroundColor = 'black';
-        }
-
+        squareDiv.style.height = `${squareSize}px`;
         squareDivsContainer.appendChild(squareDiv);
+        squareDiv.onmouseover = function() {
+            if (colorMode === 'black'){
+                squareDiv.style.backgroundColor = 'black';
+            }
+            else if (colorMode === 'rainbow'){
+                squareDiv.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
+            }
+        }
     }
 }
 
-createGrid(16);
+function randomRGB() {
+    return Math.floor(Math.random() * 256);
+}
+
+createGrid(size);
